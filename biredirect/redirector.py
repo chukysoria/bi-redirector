7c3c57e69b51033f5fd1cb749f9923a6d3fb59c6
Carlos Sanchez Mateo <chuky.soria@gmail.com>
2017-09-25 11:52:18 +0200

    Work in progress

--- biredirect/redirector.py
+++ biredirect/redirector.py
@@ -1,16 +1,58 @@
 """
 Redirector
 """
+from functools import wraps
+import json
+from os import environ as env
+from urllib.parse import urlparse
+
+from auth0.v3.authentication import GetToken, Users
 from boxsdk.exception import BoxOAuthException
-from flask import Flask, redirect, request
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, session)
 
 from biredirect.boxstores import BoxKeysStoreRedis
-from biredirect.settings import HEROKU_APP_NAME, PORT, REDIS_DB
+from biredirect.settings import (AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID,
+                                 AUTH0_CLIENT_SECRET, AUTH0_DOMAIN,
+                                 HEROKU_APP_NAME, REDIS_DB)
 
 APP = Flask(__name__)
+APP.secret_key = 'secret'  # TODO: Replace by real secret
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
+
+# Static files routes
+@APP.route('/')
+def home():
+    return render_template('home.html', env=env)
+
+@APP.route('/public/<path:filename>')
+def static_files(filename):
+    return send_from_directory('./public', filename)
 
 
-@APP.route('/redirect', methods=['GET'])
+@APP.route('/dashboard')
+@requires_auth
+def dashboard():
+    return 'Future dashboard'
+
+@APP.route('/logout')
+def logout():
+    session.clear()
+    base_url = f'https://{HEROKU_APP_NAME}.herokuapp.com/'
+    return redirect(f'https://{AUTH0_DOMAIN}/v2/logout?returnTo={base_url}&client_id={AUTH0_CLIENT_ID}')
+
+
+# API Routes
+@APP.route('/api/redirect', methods=['GET'])
 def redirect_to_box():
     """
     Redirect to the Box download address
@@ -22,21 +64,47 @@ def redirect_to_box():
     new_url = f"https://amadeus.box.com/shared/static/{doc_id}"
     return redirect(new_url)
 
+@APP.route('/api/redirect/onedrive', methods=['GET'])
+def redirect_to_onedrive():
+    """
+    Redirect to the OneDrive download address
+    """
+    filename = request.args.get('filename')
+    if filename is None:
+        new_url = "https://myoffice.accenture.com/personal/c_sanchez_mateo_accenture_com/Documents/IFTTT/Gmail/BaseExport.csv"
+    else:
+        new_url = f"https://myoffice.accenture.com/personal/c_sanchez_mateo_accenture_com/Documents/IFTTT/Gmail/{filename}"
 
-@APP.route("/authenticate")
+    return redirect(new_url)
+
+# Here we're using the /callback route.
+@APP.route('/api/authcallback')
+def callback_handling():
+    code = request.args.get('code')
+    redirectUrl = request.args.get('redirectto')
+    get_token = GetToken(AUTH0_DOMAIN)
+    auth0_users = Users(AUTH0_DOMAIN)
+    token = get_token.authorization_code(AUTH0_CLIENT_ID,
+                                         AUTH0_CLIENT_SECRET, code, AUTH0_CALLBACK_URL)
+    user_info = auth0_users.userinfo(token['access_token'])
+    session['profile'] = json.loads(user_info)
+    return redirect(redirectUrl)
+
+
+@APP.route("/api/authenticate")
 def authenticate():
     """
     Launches the authentication process
     """
     oauth = BoxKeysStoreRedis.get_oauth()
-    redirect_url = f'https://{HEROKU_APP_NAME}.herokuapp.com/boxauth'
+    redirect_url = f'https://{HEROKU_APP_NAME}.herokuapp.com/api/boxauth'
     auth_url, csrf_token = oauth.get_authorization_url(redirect_url)
     REDIS_DB.hset('box', 'csrf_token', csrf_token)
     # Redirect to Box Oauth
     return redirect(auth_url)
 
 
-@APP.route("/boxauth")
+@APP.route("/api/boxauth")
 def boxauth():
     """
     Stores the received tokens in oauth.
@@ -52,7 +120,3 @@ def boxauth():
     except AssertionError:
         response = "Tokens don't match"
     return response
-
-
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=PORT, debug=True)
--- tests/biredirect/test_redirector.py
+++ tests/biredirect/test_redirector.py
@@ -1,35 +1,77 @@
 """
 Tests for Redirector app
 """
-from biredirect.settings import HEROKU_APP_NAME
+import pytest
+
+from biredirect.settings import (AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID,
+                                 AUTH0_CLIENT_SECRET, AUTH0_DOMAIN,
+                                 HEROKU_APP_NAME)
 
 
 def test_redirect_to_box(webapp):
-    response = webapp.get('/redirect')
+    response = webapp.get('/api/redirect')
 
     assert response.status_code == 200
     assert response.data.decode() == 'a'
 
 
 def test_redirect_to_box_success(webapp):
-    response = webapp.get('/redirect?docID=abc')
+    response = webapp.get('/api/redirect?docID=abc')
 
     assert response.status_code == 302
     assert response.location == 'https://amadeus.box.com/shared/static/abc'
 
 
+def test_redirect_to_onedrive(webapp):
+    response = webapp.get('/api/redirect/onedrive')
+
+    assert response.status_code == 302
+
+
+def test_onedrive_success(webapp):
+    response = webapp.get('/api/redirect/onedrive?filename=abc')
+
+    assert response.status_code == 302
+    assert response.location == 'https://myoffice.accenture.com/personal/c_sanchez_mateo_accenture_com/Documents/IFTTT/Gmail/abc'
+
+
+@pytest.mark.parametrize("redirectTo, expectedUrl", [
+    (None, "http://localhost/"),
+    ("dashboard", "http://localhost/dashboard")
+])
+def test_callback_handling(webapp, get_token, token_instance, users, users_instance, redirectTo, expectedUrl):
+    url = "/api/authcallback?code=auth0code"
+    url = f'{url}&redirectto={redirectTo}' if redirectTo else url
+    response = webapp.get(url)
+
+    get_token.assert_called_once_with(AUTH0_DOMAIN)
+    users.assert_called_once_with(AUTH0_DOMAIN)
+    token_instance.authorization_code.assert_called_once_with(
+        AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, 'auth0code', AUTH0_CALLBACK_URL)
+    users_instance.userinfo.assert_called_once_with('at')
+    assert response.status_code == 302
+    assert response.location == expectedUrl
+
+
+def test_logout(webapp):
+    response = webapp.get('/logout')
+
+    assert response.status_code == 302
+    assert response.location == f'https://{AUTH0_DOMAIN}/v2/logout?returnTo=https://{HEROKU_APP_NAME}.herokuapp.com/&client_id={AUTH0_CLIENT_ID}'
+
+
 def test_authenticate(webapp, box_redis_store, oauth, redisdb):
-    response = webapp.get('/authenticate')
+    response = webapp.get('/api/authenticate')
 
     box_redis_store.get_oauth.assert_called_once_with()
     oauth.get_authorization_url.assert_called_once_with(
-        f'https://{HEROKU_APP_NAME}.herokuapp.com/boxauth')
+        f'https://{HEROKU_APP_NAME}.herokuapp.com/api/boxauth')
     redisdb.hset.assert_called_once_with('box', 'csrf_token', 'csrf_0123')
     assert response.location == 'http://auth_url'
 
 
 def test_box_auth(webapp, box_redis_store, redisdb, oauth):
-    response = webapp.get('/boxauth?state=csrf_0123&code=auth_0123')
+    response = webapp.get('/api/boxauth?state=csrf_0123&code=auth_0123')
 
     redisdb.hget.assert_called_once_with('box', 'csrf_token')
     box_redis_store.get_oauth.assert_called_once_with()
@@ -40,7 +82,7 @@ def test_box_auth(webapp, box_redis_store, redisdb, oauth):
 
 
 def test_box_auth_failure(webapp, redisdb):
-    response = webapp.get('/boxauth?state=csrf_1234&code=auth_0123')
+    response = webapp.get('/api/boxauth?state=csrf_1234&code=auth_0123')
     redisdb.hget.assert_called_once_with('box', 'csrf_token')
 
     assert response.status_code == 200
