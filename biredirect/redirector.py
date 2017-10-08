3fba270d2b9f2de1e2481c59c299e0aa91ae1032
Carlos Sanchez Mateo <chuky.soria@gmail.com>
2017-10-08 13:24:35 +0200

    Backed - Switch to JWT authentication for API - Reorder Box endpoint - Fixed static routing

--- biredirect/redirector.py
+++ biredirect/redirector.py
@@ -8,6 +8,9 @@ from auth0.v3.authentication import GetToken, Users
 from boxsdk.exception import BoxOAuthException
 from flask import (Flask, jsonify, redirect, request, send_from_directory,
                    session)
+import jwt
+from jwt.algorithms import RSAAlgorithm
+import requests
 
 from biredirect.boxstores import BoxKeysStoreRedis
 from biredirect.dbservice import DBService
@@ -21,15 +24,39 @@ DB = DBService()
 
 
 # Login decorator
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if 'profile' not in session:
-            # Redirect to Login page here
-            return redirect('/')
-        return f(*args, **kwargs)
+def jwt_required(function):
+    @wraps(function)
+    def wrapper(*orig_args, **orig_kwargs):
+        # Return if testing
+        if APP.config['TESTING']:
+            return function(*orig_args, **orig_kwargs)
+        # Get Bearer token
+        access_token = request.headers.get('Authorization')
+        if access_token:
+            access_token = access_token.replace('Bearer ', '', 1)
+            # Obtain first JWK and the keys to validate the signature
+            jwks_object = requests.get(
+                f"https://{AUTH0_DOMAIN}/.well-known/jwks.json").json()
+            jwks_key = RSAAlgorithm.from_jwk(json.dumps(jwks_object['keys'][0]))
+            try:
+                payload = jwt.decode(
+                    access_token, jwks_key, algorithms=['RS256'],
+                    audience=f'https://{AUTH0_DOMAIN}/api/v2/',
+                    issuer=f"https://{AUTH0_DOMAIN}/")
+                if payload['sub']:
+                    return function(*orig_args, **orig_kwargs)
+                return route_error(401, "Unauthorized user")
+            except jwt.ExpiredSignatureError:
+                return route_error(401, "Expired Token")
+        return route_error(401, "Authorization needed")
+    return wrapper
 
-    return decorated
+
+def route_error(code, message):
+    print('Error!:', code, message)
+    response = jsonify({'error': message})
+    response.status_code = code
+    return response
 
 
 # API Routes
@@ -47,6 +74,7 @@ def redirect_to_box():
 
 
 @APP.route('/api/configs', methods=['POST'])
+@jwt_required
 def create_config():
     config = DB.insert_config(request.json)
     if config:
@@ -55,6 +83,7 @@ def create_config():
 
 
 @APP.route('/api/configs', methods=['GET'])
+@jwt_required
 def retrieve_configs():
     """
     Return all configs
@@ -63,6 +92,7 @@ def retrieve_configs():
 
 
 @APP.route('/api/configs/<int:config_id>', methods=['GET'])
+@jwt_required
 def retrieve_config(config_id):
     config = DB.get_config(config_id)
     if config:
@@ -71,6 +101,7 @@ def retrieve_config(config_id):
 
 
 @APP.route('/api/configs/<int:config_id>', methods=['PUT'])
+@jwt_required
 def update_config(config_id):
     config = DB.update_config(config_id, request.json)
     if config:
@@ -79,6 +110,7 @@ def update_config(config_id):
 
 
 @APP.route('/api/configs/<int:config_id>', methods=['DELETE'])
+@jwt_required
 def delete_config(config_id):
     if DB.delete_config(config_id):
         return jsonify({'result': 'success'})
@@ -108,6 +140,7 @@ def callback_handling():
 
 
 @APP.route('/logout')
+@jwt_required
 def logout():
     session.clear()
     base_url = f'https://{HEROKU_APP_NAME}.herokuapp.com/'
@@ -117,20 +150,21 @@ def logout():
 
 
 # Box logins
-@APP.route("/api/authenticate")
+@APP.route("/api/box/authenticate")
+@jwt_required
 def authenticate():
     """
     Launches the authentication process
     """
     oauth = BoxKeysStoreRedis.get_oauth()
-    redirect_url = f'https://{HEROKU_APP_NAME}.herokuapp.com/api/boxauth'
+    redirect_url = f'https://{HEROKU_APP_NAME}.herokuapp.com/api/box/callback'
     auth_url, csrf_token = oauth.get_authorization_url(redirect_url)
     DB.set_crsf_token(csrf_token)
     # Redirect to Box Oauth
     return redirect(auth_url)
 
 
-@APP.route("/api/boxauth")
+@APP.route("/api/box/callback")
 def boxauth():
     """
     Stores the received tokens in oauth.
@@ -151,16 +185,10 @@ def boxauth():
 
 
 # Catch All urls, enabling copy-paste url
-@APP.route('/', defaults={'path': ''})
-@APP.route('/<path:path>')  # Catch All urls, enabling copy-paste url
-def home(path):
-    print('here: %s' % path)
-    if path == '':
-        path = 'index.html'
-    return send_from_directory('./dist', path)
-
-
-@APP.route('/dashboard')
-@requires_auth
-def dashboard():
-    return 'Future dashboard'
+@APP.route('/', defaults={'location': ''})
+@APP.route('/<path:location>')  # Catch All urls, enabling copy-paste url
+def home(location):
+    print('here: %s' % location)
+    if location == '' or '.' not in location:
+        location = 'index.html'
+    return send_from_directory('./dist', location)
